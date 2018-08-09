393362bba9493cb394604f596b12169bab9f4de9
walker <kxh@meitu.com>
2018-08-09 15:55:57 +0800

    ADD: peeraddr and it's test

---
+++ p2p/dht/peeraddr.go
@@ -0,0 +1,45 @@
+package dht
+
+import (
+	"strings"
+	"errors"
+	"encoding/hex"
+)
+
+type peeraddr = string
+
+var(
+	errStrAddressEmpty = errors.New("address was empty")
+	errStrInvalidAddress       = errors.New("invalid address")
+)
+
+func ParsePeerAddr(addr peeraddr) (*PeerID, error) {
+
+	addr = strings.TrimSpace(addr)
+	if len(addr) == 0 {
+		return nil, errStrAddressEmpty
+	}
+
+	subAddrs := strings.Split(addr, "@")
+	if len(subAddrs) != 2 {
+		return nil, errStrInvalidAddress
+	}
+
+	pubk := subAddrs[0]
+	url := subAddrs[1]
+	pbPubk, err:= hex.DecodeString(pubk)
+	if err != nil {
+		return nil, err
+	}
+
+	id := CreateID(url, pbPubk)
+	return &id, nil
+
+}
+
+func FormatPeerAddr(id PeerID) peeraddr {
+
+	subAddrs := []string{id.PublicKeyHex(), id.ID.Address}
+	return strings.Join(subAddrs, "@")
+}
+
---
+++ p2p/dht/peeraddr_test.go
@@ -0,0 +1,39 @@
+package dht
+
+import (
+	"testing"
+
+	crypto "github.com/libp2p/go-libp2p-crypto"
+	"fmt"
+	"encoding/hex"
+)
+
+func TestFormatAddr(t *testing.T) {
+	t.Parallel()
+
+	key, _ := GenerateKey(crypto.Secp256k1)
+	pbKey,_ := key.GetPublic().Bytes()
+
+	peerA := CreateID("/ip4/127.0.0.1/tcp/8860", pbKey)
+
+	address := FormatPeerAddr(peerA)
+
+	expected := fmt.Sprintf("%s@%s", hex.EncodeToString(pbKey),"/ip4/127.0.0.1/tcp/8860")
+	if address != expected {
+		t.Errorf("FormatPeerAddr() = %+v, expected %+v\n", address, expected)
+	}
+	t.Logf("address :%s\n", address)
+}
+
+func TestParsePeerAddr(t *testing.T) {
+	t.Parallel()
+
+	address := peeraddr("08021221022c8c52f66009dd50396dc49876742d37b26ddb5cb83006ee417112cf2da7edd7@/ip4/127.0.0.1/tcp/8860")
+
+	id, err := ParsePeerAddr(address)
+	if err != nil {
+		t.Errorf("ParseAddress() = <nil>, expected %+v (%s)\n", err, address)
+	}
+
+	t.Logf("%s\n", id.String())
+}
